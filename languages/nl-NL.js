588a170efee6ff8abe2fbadf20a41136a0347c3b
Tjird <35243474+Tjird@users.noreply.github.com>
2019-01-28 16:22:37 +0100

    Create Dutch translation
    Created a file for the Dutch translation

---
+++ languages/nl-NL.js
@@ -0,0 +1,38 @@
+ //dutch (the netherlands)
+module.exports = { 
+  "endings": {
+    "0":"0ste",
+    "1":"1ste",
+    "2":"2de",
+    "3":"3de",
+    "4":"4de",
+    "5":"5de",
+    "6":"6de",
+    "7":"7de",
+    "8":"8de",
+    "9":"9de"
+  },
+    "days": {
+      "Sun":"Zondag",
+      "Mon":"Maandag",
+      "Tue":"Dinsdag",
+      "Wed":"Woensdag",
+      "Thu":"Donderdag",
+      "Fri":"Vrijdag",
+      "Sat":"Zaterdag"
+   },
+    "months": {
+      "Jan":"Januari",
+      "Feb":"Februari",
+      "Mar":"Maart",
+      "Apr":"April",
+      "May":"Mei",
+      "Jun":"Juni",
+      "Jul":"Juli",
+      "Aug":"Augustus",
+      "Sep":"September",
+      "Oct":"Oktober",
+      "Nov":"November",
+      "Dec":"December"
+    }
+}
